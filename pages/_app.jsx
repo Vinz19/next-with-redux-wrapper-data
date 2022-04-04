7279927136c3f55f1cee66bae9effccb35de9084
Vinz19 <vinzparis@mail.fr>
2022-04-04 20:13:55 +0200

    Modified _app and import Redux Wrapper

---
+++ pages/_app.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { wrapper } from "../redux/store";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default wrapper.withRedux(MyApp);
